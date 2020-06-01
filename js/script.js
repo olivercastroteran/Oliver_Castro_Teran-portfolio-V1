d3cdf020258c0b2fcb75f9a1239911ac6d33ea2f
olivercastroteran <olivercastroteran@gmail.com>
2020-06-01 19:12:52 -0400

    skills section updated

--- js/script.js
+++ js/script.js
@@ -7,5 +7,16 @@ window.addEventListener('scroll', () => {
   if (y + 400 > skillsOffset) {
     const skills = document.querySelectorAll('.skill__bar-fill');
     skills.forEach((skill) => skill.classList.add('load'));
+
+    const percentages = document.querySelectorAll('.skill__percentage');
+    percentages.forEach((percentage) => (percentage.style.opacity = '1'));
+  }
+
+  if (y + 400 < skillsOffset) {
+    const skills = document.querySelectorAll('.skill__bar-fill');
+    skills.forEach((skill) => skill.classList.remove('load'));
+
+    const percentages = document.querySelectorAll('.skill__percentage');
+    percentages.forEach((percentage) => (percentage.style.opacity = '0'));
   }
 });
